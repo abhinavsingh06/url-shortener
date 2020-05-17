d1bd1831dc0858ef38cc048e6e6539add4d4525f
Abhinav Singh <singh.abhinav6996@gmail.com>
2020-05-18 03:40:12 +0530

    Hello React

--- app/javascript/components/List.jsx
+++ app/javascript/components/List.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function List() {
-  return <div>welcome........again list</div>;
+  return <div>Hello React!</div>;
 }
