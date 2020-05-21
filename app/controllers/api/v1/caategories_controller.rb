7d99674c708ce4ff6768df0ecefc0ef45fdc4749
Abhinav Singh <singh.abhinav6996@gmail.com>
2020-05-22 03:45:38 +0530

    Add actions for categories controller

--- app/controllers/api/v1/caategories_controller.rb
+++ app/controllers/api/v1/caategories_controller.rb
@@ -1,13 +1,46 @@
 class Api::V1::CaategoriesController < ApplicationController
   def index
+    @categories = Category.order(updated_at: :desc)
+    render status: :ok, json: { categories: @categories }
   end
 
   def create
+    @category = Category.new(category_params)
+    if @category.save
+      render status: :ok, json: { notice: 'Category was successfully created', name: @category.name }
+    else 
+      render status: :unprocessable_entity, json: { errors: @category.errors.full_messages }
+    end
   end
 
   def show
+    @category = Category.where(name: params[:name]).first
+    unless @category
+      redirect_to controller: "categories", action: "index"
+    end
+  end
+
+  def update
+    @category = Category.where(name: params[:name]).first
+    if @category.update_attributes(category_params)
+      render status: :ok, json: { notice: "Successfully updated category." , name: @category.name}
+    else
+      render status: :unprocessable_entity, json:{ errors: @category.errors.full_messages }
+    end
   end
 
   def destroy
+    @category = Category.where(name: params[:name]).first
+    if @category.destroy
+      render status: :ok, json: { notice: "Successfully deleted category." }
+    else
+      render status: :unprocessable_entity, json: { errors: @task.errors.full_messages }
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :color)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :urls, only: [:index, :create, :show, :update], param: :short
-      resources :categories, param: :name
+      resources :categories, only: [:index, :create, :show, :update, :destroy], param: :name
     end
   end
   root 'static#index'
