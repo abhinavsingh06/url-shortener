c24c8102b1da5192c1eeafc6bb90c3cffe52854b
Abhinav Singh <singh.abhinav6996@gmail.com>
2020-05-22 02:11:47 +0530

    add controlller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :urls, only: [:index, :create, :show, :update], param: :short
+      resources :categories, param: :name
     end
   end
   root 'static#index'
