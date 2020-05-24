fe7e366d2c68a1671df06ae763b2e91eeaa55afc
Abhinav Singh <singh.abhinav6996@gmail.com>
2020-05-25 00:07:37 +0530

    add model tests

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "name should be present" do
+    name = Category.new(name: "")
+    assert name.invalid?
+  end
+
+  test "name should be unique" do
+    name1 = categories(:one)
+    name2 = Category.new(name: "xyz")
+    assert name2.invalid?
+  end
+
+  test "color should have length 8" do
+    color = Category.new(color: "#gh")
+    assert color.invalid?
+  end
+
+  test "name should not be duplicate" do
+    name1 = categories(:one)
+    name2 = Category.new(name: 'xyz')
+    assert name2.invalid?
+  end
 end
