5c0cb19f844f5bd96a739f92ab1022941fdbf5e2
Abhinav Singh <singh.abhinav6996@gmail.com>
2020-05-30 02:25:00 +0530

    fix controller

--- app/controllers/api/v1/urls_controller.rb
+++ app/controllers/api/v1/urls_controller.rb
@@ -24,7 +24,7 @@ class Api::V1::UrlsController < ApplicationController
 
   def show
     @url = Url.find_by_short(params[:short])
-    @url.update(count: @url.count + 1)
+    @url.update_attributes(count: @url.count + 1)
     if @url
       if @url.category
         render status: :ok, json: { original: @url.original, category: @url.category }
